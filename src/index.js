5184e7c8f9f497dbdbf4b19cc0921ce436808aaa
Luke <gksdnr89@gmail.com>
2022-08-31 21:53:57 +0900

    Update jsdoc

--- src/index.js
+++ src/index.js
@@ -1,51 +1,22 @@
 /**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
+ * @class
+ * @property {number} id        - 아이디(required)
+ * @property {string} content   - 내용(required)
+ * @property {boolean} isDone   - 완료여부(required)
+ * @property {string} category  - 카테고리(required)
+ * @property {Tag[]|undefined} [tags=null]     - 태그들(optional)
  */
-function Book(title, author)   {}
+class Todo {}
 
 /**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
+ * @class
+ * @property {number} id    - 아이디(required)
+ * @property {string} name  - 태그명(requried)
  */
-function foo() {}
-
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
- */
-function bar() {}
+class Tag {}
 
 /**
- * Generic dairy product.
- * @constructor
+ * @class
+ * @property {Todo[]} todos - 할일들
  */
-function DairyProduct() {}
-
-/**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
- */
-DairyProduct.prototype.isSolid = function () {
-  throw new Error('must be implemented by subclass!')
-}
-
-/**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
- */
-function Milk() {}
-
-/**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
- */
-Milk.prototype.isSolid = function () {
-  return false
-}
+class TodoManager {}
